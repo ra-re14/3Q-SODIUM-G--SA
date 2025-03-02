a54c4cdb199575d6b503c07a8c6ea37787e30576
Raul II A. Recla <rarecla22@src.pshs.edu.ph>
2025-03-02 17:43:30 +0800

    Add files via upload

---
+++ wordleGame.js
@@ -0,0 +1,143 @@
+var chances = 5;
+var words = ['night', 'fires'];
+var selectedWord = words[Math.round(Math.random(0,1))].toUpperCase();
+let row = 0;
+let win = false;
+
+console.log(selectedWord);
+
+function playGame(){
+    if (chances == 0 && win === false){
+        alert('you already lost please refresh the page');
+        return;
+    }
+    if (win === true){
+        alert('you already won please refresh the page');
+        return;
+    }
+    let typedWord = (prompt('type a word')).toUpperCase();
+
+    if(typedWord==selectedWord){
+        win = true;
+    }
+
+    if (typedWord.length == 5){
+
+        let box = (row.toString()).concat('-0')
+        document.getElementById(box).innerHTML = typedWord[0];
+        if (typedWord[0] === selectedWord[0]){
+            document.getElementById(box).style.backgroundColor = 'green';
+        }else
+        if (typedWord[0] === selectedWord[1]){
+            document.getElementById(box).style.backgroundColor = 'yellow';
+        }else
+        if (typedWord[0] === selectedWord[2]){
+            document.getElementById(box).style.backgroundColor = 'yellow';
+        }else
+        if (typedWord[0] === selectedWord[3]){
+            document.getElementById(box).style.backgroundColor = 'yellow';
+        }else
+        if (typedWord[0] === selectedWord[4]){
+            document.getElementById(box).style.backgroundColor = 'yellow';
+        }else{
+            document.getElementById(box).style.backgroundColor = 'gray';
+        }
+
+        box = (row.toString()).concat('-1')
+        document.getElementById(box).innerHTML = typedWord[1];
+        if (typedWord[1] === selectedWord[1]){
+            document.getElementById(box).style.backgroundColor = 'green';
+        }else
+        if (typedWord[1] === selectedWord[0]){
+            document.getElementById(box).style.backgroundColor = 'yellow';
+        }else
+        if (typedWord[1] === selectedWord[2]){
+            document.getElementById(box).style.backgroundColor = 'yellow';
+        }else
+        if (typedWord[1] === selectedWord[3]){
+            document.getElementById(box).style.backgroundColor = 'yellow';
+        }else
+        if (typedWord[1] === selectedWord[4]){
+            document.getElementById(box).style.backgroundColor = 'yellow';
+        }else{
+            document.getElementById(box).style.backgroundColor = 'gray';
+        }
+
+        box = (row.toString()).concat('-2')
+        document.getElementById(box).innerHTML = typedWord[2];
+        if (typedWord[2] === selectedWord[2]){
+            document.getElementById(box).style.backgroundColor = 'green';
+        }else
+        if (typedWord[2] === selectedWord[1]){
+            document.getElementById(box).style.backgroundColor = 'yellow';
+        }else
+        if (typedWord[2] === selectedWord[0]){
+            document.getElementById(box).style.backgroundColor = 'yellow';
+        }else
+        if (typedWord[2] === selectedWord[3]){
+            document.getElementById(box).style.backgroundColor = 'yellow';
+        }else
+        if (typedWord[2] === selectedWord[4]){
+            document.getElementById(box).style.backgroundColor = 'yellow';
+        }else{
+            document.getElementById(box).style.backgroundColor = 'gray';
+        }
+
+        box = (row.toString()).concat('-3')
+        document.getElementById(box).innerHTML = typedWord[3];
+        if (typedWord[3] === selectedWord[3]){
+            document.getElementById(box).style.backgroundColor = 'green';
+        }else
+        if (typedWord[3] === selectedWord[1]){
+            document.getElementById(box).style.backgroundColor = 'yellow';
+        }else
+        if (typedWord[3] === selectedWord[2]){
+            document.getElementById(box).style.backgroundColor = 'yellow';
+        }else
+        if (typedWord[3] === selectedWord[0]){
+            document.getElementById(box).style.backgroundColor = 'yellow';
+        }else
+        if (typedWord[3] === selectedWord[4]){
+            document.getElementById(box).style.backgroundColor = 'yellow';
+        }else{
+            document.getElementById(box).style.backgroundColor = 'gray';
+        }
+
+        box = (row.toString()).concat('-4')
+        document.getElementById(box).innerHTML = typedWord[4];
+        if (typedWord[4] === selectedWord[4]){
+            document.getElementById(box).style.backgroundColor = 'green';
+        }else
+        if (typedWord[4] === selectedWord[1]){
+            document.getElementById(box).style.backgroundColor = 'yellow';
+        }else
+        if (typedWord[4] === selectedWord[2]){
+            document.getElementById(box).style.backgroundColor = 'yellow';
+        }else
+        if (typedWord[4] === selectedWord[3]){
+            document.getElementById(box).style.backgroundColor = 'yellow';
+        }else
+        if (typedWord[4] === selectedWord[0]){
+            document.getElementById(box).style.backgroundColor = 'yellow';
+        }else{
+            document.getElementById(box).style.backgroundColor = 'gray';
+        }
+        
+
+        row += 1;
+        chances -= 1;
+    }else{
+        alert('word length is greater or less than 5')
+    }
+
+    if (chances == 0 && win === false){
+        alert('you lost');
+        return;
+    }
+    if (win === true){
+        alert('you won');
+        return;
+    }
+
+
+}
